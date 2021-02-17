ed25012131ad444074702d8118f404908df39ebc
Jonas De Ridder <jonasderidder64@gmail.com>
2021-02-17 12:43:10 +0100

    Add Input Commands

--- Minigin/InputManager.cpp
+++ Minigin/InputManager.cpp
@@ -7,6 +7,7 @@ bool dae::InputManager::ProcessInput()
 {
 	ZeroMemory(&m_CurrentState, sizeof(XINPUT_STATE));
 	XInputGetState(0, &m_CurrentState);
+	UpdateControllerCommandMap();
 
 	SDL_Event e;
 	while (SDL_PollEvent(&e)) {
@@ -24,19 +25,87 @@ bool dae::InputManager::ProcessInput()
 	return true;
 }
 
-bool dae::InputManager::IsPressed(ControllerButton button) const
+dae::InputState dae::InputManager::IsPressed(unsigned id, ControllerButton button) const
 {
-	switch (button)
+	auto buttonIt = std::find_if(m_Controls.begin(), m_Controls.end(), [id, button](const std::pair<ControllerKey, ControllerCommand>& pair)
 	{
-	case ControllerButton::ButtonA:
-		return m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_A;
-	case ControllerButton::ButtonB:
-		return m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_B;
-	case ControllerButton::ButtonX:
-		return m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_X;
-	case ControllerButton::ButtonY:
-		return m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_Y;
-	default: return false;
+			return pair.first.first == id && pair.first.second == button;
+	});
+
+	if (buttonIt == m_Controls.end()) return InputState::up;
+	const ButtonInfo& buttonInfo = buttonIt->second.first;
+
+	if (buttonInfo.pressed && !buttonInfo.lastValue) return InputState::pressed;
+	if (buttonInfo.pressed && buttonInfo.lastValue) return InputState::down;
+	if (!buttonInfo.pressed && buttonInfo.lastValue) return InputState::released;
+	return InputState::up;
+}
+
+void dae::InputManager::Bind(ControllerButton button, const std::shared_ptr<Command>& command)
+{
+	//For loop in case there are multiple controllers
+	for (auto& pair : m_Controls)
+	{
+		if (pair.first.second == button) pair.second.second = command;
+	}
+}
+
+void dae::InputManager::UpdateControllerCommandMap()
+{
+	for (auto& pair : m_Controls)
+	{
+		ControllerCommand& cc = pair.second;
+		ButtonInfo& bi = cc.first;
+		bi.lastValue = bi.pressed;
+
+		const ControllerKey& ck = pair.first;
+		switch (ck.second)
+		{
+		case ControllerButton::ButtonA:
+			cc.first.pressed = m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_A;
+			break;
+		case ControllerButton::ButtonB:
+			cc.first.pressed = m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_B;
+			break;
+		case ControllerButton::ButtonX:
+			cc.first.pressed = m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_X;
+			break;
+		case ControllerButton::ButtonY:
+			cc.first.pressed = m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_Y;
+			break;
+		case ControllerButton::ButtonStart:
+			cc.first.pressed = m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_START;
+			break;
+		case ControllerButton::ButtonBack:
+			cc.first.pressed = m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_BACK;
+			break;
+		case ControllerButton::ShoulderLeft:
+			cc.first.pressed = m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER;
+			break;
+		case ControllerButton::ShoulderRight:
+			cc.first.pressed = m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER;
+			break;
+		case ControllerButton::DpadLeft:
+			cc.first.pressed = m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_LEFT;
+			break;
+		case ControllerButton::DpadRight:
+			cc.first.pressed = m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_RIGHT;
+			break;
+		case ControllerButton::DpadUp:
+			cc.first.pressed = m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_UP;
+			break;
+		case ControllerButton::DpadDown:
+			cc.first.pressed = m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_DOWN;
+			break;
+		case ControllerButton::ThumbLeft:
+			cc.first.pressed = m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_LEFT_THUMB;
+			break;
+		case ControllerButton::ThumbRight:
+			cc.first.pressed = m_CurrentState.Gamepad.wButtons & XINPUT_GAMEPAD_RIGHT_THUMB;
+			break;
+		default:
+			cc.first.pressed = false;
+		}
 	}
 }
 
--- Minigin/InputManager.h
+++ Minigin/InputManager.h
@@ -1,5 +1,8 @@
 #pragma once
+#include <map>
 #include <XInput.h>
+
+#include "Command.h"
 #include "Singleton.h"
 
 namespace dae
@@ -9,16 +12,50 @@ namespace dae
 		ButtonA,
 		ButtonB,
 		ButtonX,
-		ButtonY
+		ButtonY,
+		ButtonStart,
+		ButtonBack,
+		ShoulderRight,
+		ShoulderLeft,
+		DpadLeft,
+		DpadRight,
+		DpadUp,
+		DpadDown,
+		ThumbLeft,
+		ThumbRight,
+		Last
+	};
+
+	enum class InputState
+	{
+		pressed,
+		released,
+		down,
+		up
 	};
 
 	class InputManager final : public Singleton<InputManager>
 	{
 	public:
 		bool ProcessInput();
-		bool IsPressed(ControllerButton button) const;
+		InputState IsPressed(unsigned id, ControllerButton button) const;
+		void Bind(ControllerButton button, const std::shared_ptr<Command>& command);
 	private:
 		XINPUT_STATE m_CurrentState{};
+
+		struct ButtonInfo
+		{
+			bool pressed;
+			bool lastValue;
+		};
+
+		void UpdateControllerCommandMap();
+		
+		//unsigned = controller id
+		using ControllerKey = std::pair<unsigned, ControllerButton>;
+		using ControllerCommand = std::pair<ButtonInfo, std::shared_ptr<Command>>;
+		using ControllerCommandMap = std::map<ControllerKey, ControllerCommand>;
+		ControllerCommandMap m_Controls{};
 	};
 
 }
