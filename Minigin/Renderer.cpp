343457f2d1db735954bf89bdceee9bf54856ce39
Jonas De Ridder <jonasderidder64@gmail.com>
2021-02-17 14:24:04 +0100

    Add ImGui

--- Minigin/Renderer.cpp
+++ Minigin/Renderer.cpp
@@ -1,16 +1,31 @@
 #include "MiniginPCH.h"
 #include "Renderer.h"
-#include <SDL.h>
+
 #include "SceneManager.h"
 #include "Texture2D.h"
+#pragma warning(push)
+#pragma warning(disable:26495)
+#pragma warning(disable:26451)
+#include <SDL.h>
+#include "imgui.h"
+
+#include "backends/imgui_impl_opengl2.h"
+#include "backends/imgui_impl_sdl.h"
+#pragma warning(pop)
 
 void dae::Renderer::Init(SDL_Window * window)
 {
+	m_pWindow = window;
 	m_Renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (m_Renderer == nullptr) 
 	{
 		throw std::runtime_error(std::string("SDL_CreateRenderer Error: ") + SDL_GetError());
 	}
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplSDL2_InitForOpenGL(window, SDL_GL_GetCurrentContext());
+	ImGui_ImplOpenGL2_Init();
 }
 
 void dae::Renderer::Render() const
@@ -18,12 +33,28 @@ void dae::Renderer::Render() const
 	SDL_RenderClear(m_Renderer);
 
 	SceneManager::GetInstance().Render();
+
+	ImGui_ImplOpenGL2_NewFrame();
+	ImGui_ImplSDL2_NewFrame(m_pWindow);
+	ImGui::NewFrame();
+	if (m_ShowDemo)
+	{
+		bool showDemo{ m_ShowDemo };
+		ImGui::ShowDemoWindow(&showDemo);
+	}
+	
+	ImGui::Render();
+	ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
 	
 	SDL_RenderPresent(m_Renderer);
 }
 
 void dae::Renderer::Destroy()
 {
+	ImGui_ImplOpenGL2_Shutdown();
+	ImGui_ImplSDL2_Shutdown();
+	ImGui::DestroyContext();
+	
 	if (m_Renderer != nullptr)
 	{
 		SDL_DestroyRenderer(m_Renderer);
