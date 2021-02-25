a6715ab70ce2494fa6e37c2d1d506b5930645222
Jonas De Ridder <jonasderidder64@gmail.com>
2021-02-25 17:34:56 +0100

    Components save component/gameObject they should execute on

--- Minigin/Command.cpp
+++ Minigin/Command.cpp
@@ -1,2 +1,14 @@
 #include "MiniginPCH.h"
 #include "Command.h"
+
+dae::Command::Command(GameObject* pDestination)
+	: m_pGameObject{pDestination}
+	, m_pComponent{nullptr}
+{
+}
+
+dae::Command::Command(Component* pDestination)
+	: m_pComponent{pDestination}
+	, m_pGameObject{nullptr}
+{
+}
--- Minigin/Command.h
+++ Minigin/Command.h
@@ -1,8 +1,21 @@
 #pragma once
-class Command
-{
-public:
-	virtual ~Command() = default;
-	virtual void Execute() = 0;
-};
 
+namespace dae
+{
+	class Component;
+	class GameObject;
+
+	class Command
+	{
+	public:
+		Command() = default;
+		Command(GameObject* pDestination);
+		Command(Component* pDestination);
+		virtual ~Command() = default;
+		virtual void Execute() = 0;
+	protected:
+		GameObject* m_pGameObject;
+		Component* m_pComponent;
+	};
+
+}
